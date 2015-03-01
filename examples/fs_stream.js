c8b678994058e138d1baa3fa85f7be5738062e05
hex7c0 <carniellifrancesco@gmail.com>
2015-03-01 11:35:05 +0100

    examples: add stream

---
+++ examples/fs_stream.js
@@ -0,0 +1,32 @@
+'use strict';
+/**
+ * @file fs stream example
+ * @module mongodb-restore
+ * @subpackage examples
+ * @version 0.0.0
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+var restore = require('..'); // use require('mongodb-restore') instead
+var fs = require('fs');
+
+/*
+ * use
+ */
+// simulate filesystem stream
+var stream = fs.createReadStream('download.tar');
+
+restore({
+  uri: 'uri', // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
+  stream: stream,
+  tar: 'dump.tar', // just for enable tar
+  callback: function() {
+
+    console.log('done');
+  }
+});
